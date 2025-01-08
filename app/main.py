faf6110cce5a81ff3d4303760b13a430b09ce546
GitSiddhant <siddhantjain778@gmail.com>
2025-01-09 01:34:00 +0530

    Parse correlation ID

--- app/main.py
+++ app/main.py
@@ -2,21 +2,17 @@ import socket  # noqa: F401
 
 
 def create_message(correlationId):
-    
-    return len("0").to_bytes(4, byteorder="big") + correlationId
+    idByte = correlationId.to_bytes(4, byteorder="big")
+    return len(idByte).to_bytes(4, byteorder="big") + idByte
+
 def handle_client(client):
     s = client.recv(1024)
-    correlationId = s[16:24]
-    
+    correlationId = int.from_bytes(s[8:12], byteorder="big")   
     client.sendall(create_message(correlationId))
     client.close()
-def main():
-    # You can use print statements as follows for debugging,
-    # they'll be visible when running tests.
-    print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
+def main():
+    print("Logs from your program will appear here!")
     server = socket.create_server(("localhost", 9092), reuse_port=True)
     print("Server is listening on port 9092")
     while True:
